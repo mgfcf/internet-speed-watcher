b4f15a6e46e038cac6c30828073701d6c10366ac
Max <m.giller.dev@gmail.com>
2021-09-10 00:11:04 +0200

    Add line break to header

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 CSV_SEP = ";"
 CSV_FILE = "speed_log.csv"
-CSV_HEADER = "Timestmap;Ping [ms];Download [bps];Upload [bps];Connections;JSON"
+CSV_HEADER = "Timestmap;Ping [ms];Download [bps];Upload [bps];Connections;JSON\n"
 
 TEST_DELAY = 600    # sec
